5c52984e5dac3adfac1002fb344c392ed5ed8305
rjgates3 <gates.j.richard@gmail.com>
2019-08-31 17:31:10 -0600

    cleanup

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    PORT: process.env.PORT || 8000,
+    PORT: process.env.PORT || 8080,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DB_URL: process.env.DB_URL || 'postgresql://dunder-mifflin:pass@localhost/bookmarks',
 };
